24667753b5d0d8e7d8ab3357dbe594a47d8355d4
Cheng Pan <cproripop@gmail.com>
2016-08-10 02:48:00 -0400

    Add comments

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ import Msg from './Msg/Msg';
 
 export { Msg };
 
+// https://github.com/leebyron/ecmascript-export-default-from
 // export { Msg } from './Msg/Msg';
--- src/containers/index.js
+++ src/containers/index.js
@@ -2,4 +2,5 @@ import App from './App/App';
 
 export { App };
 
+// https://github.com/leebyron/ecmascript-export-default-from
 // export { App } from './App/App';
