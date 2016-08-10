708d93b268a339b0dbcb60703c46be345a9f6f03
Cheng Pan <cproripop@gmail.com>
2016-08-10 02:54:03 -0400

    Fix comments

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,4 @@ import Msg from './Msg/Msg';
 export { Msg };
 
 // https://github.com/leebyron/ecmascript-export-default-from
-// export { Msg } from './Msg/Msg';
+// export Msg from './Msg/Msg';
--- src/containers/index.js
+++ src/containers/index.js
@@ -3,4 +3,4 @@ import App from './App/App';
 export { App };
 
 // https://github.com/leebyron/ecmascript-export-default-from
-// export { App } from './App/App';
+// export App from './App/App';
